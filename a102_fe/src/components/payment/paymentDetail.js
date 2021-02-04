a1f8f7cbbb2250e77dcd8c98e715553e2e88971b
BonHyuck <qhsgur0126@gmail.com>
2021-02-04 22:51:06 +0900

    Edits S04P13A102-35 : 화면 구성 및 세부사항조정

--- a102_fe/src/components/payment/paymentDetail.js
+++ a102_fe/src/components/payment/paymentDetail.js
@@ -2,45 +2,46 @@ import { Col, FormGroup, Label, Input, Button } from "reactstrap";
 import { useState, useEffect } from "react";
 
 function PaymentDetail() {
-
-    let [totalPrice, setTotalPrice] = useState(
-        localStorage.getItem("price")
-          ? JSON.parse(localStorage.getItem("price"))
-          : 0
-      );
+  let [totalPrice, setTotalPrice] = useState(
+    localStorage.getItem("price")
+      ? JSON.parse(localStorage.getItem("price"))
+      : 0
+  );
 
   return (
     <Col className="paymentDetailBox pt-2 pb-2" md="4" xs="12">
-        <Col className="paymentAmount pb-4">
-            <h3 className="text-left">총 후원 금액</h3>
-            <h2 className="text-right">{totalPrice}원</h2>
-        </Col>
-        <hr/>
-        <Col className="paymentMethod pt-4 pb-2">
-            <h3>후원 방식 선택</h3>
-            <FormGroup tag="fieldset" className="col">
-                <FormGroup className="col mt-3">
-                    <Label check>
-                        <Input type="radio" name="selectPayment" />{' '}
-                        카카오페이
-                    </Label>
-                </FormGroup>
-                <FormGroup className="col">
-                    <Label check>
-                        <Input type="radio" name="selectPayment" />{' '}
-                        네이버페이
-                    </Label>
-                </FormGroup>
-                <FormGroup className="col">
-                    <Label check>
-                        <Input type="radio" name="selectPayment" />{' '}
-                        신용/체크카드
-                    </Label>
-                </FormGroup>
-            </FormGroup>            
-        </Col>
-        <hr />
-        <Button className="paymentButton" block>후원하기</Button>
+      <Col className="paymentAmount pb-4">
+        <h3 className="text-left">총 후원 금액</h3>
+        <h2 className="text-right">{totalPrice}원</h2>
+      </Col>
+      <hr />
+      <Col className="paymentMethod pt-4 pb-2">
+        <h3>후원 방식 선택</h3>
+        <FormGroup tag="fieldset" className="col">
+          <FormGroup className="col mt-3">
+            <Label>
+              <Input type="radio" name="selectPayment" value="kakaoPay" />{" "}
+              <span>카카오페이</span>
+            </Label>
+          </FormGroup>
+          <FormGroup className="col">
+            <Label>
+              <Input type="radio" name="selectPayment" value="kakaoPay" />{" "}
+              <span>네이버페이</span>
+            </Label>
+          </FormGroup>
+          <FormGroup className="col">
+            <Label>
+              <Input type="radio" name="selectPayment" value="kakaoPay" />{" "}
+              <span>신용/체크카드</span>
+            </Label>
+          </FormGroup>
+        </FormGroup>
+      </Col>
+      <hr />
+      <Button className="paymentButton" color="primary" size="lg" block>
+        후원하기
+      </Button>
     </Col>
   );
 }
--- a102_fe/src/components/payment/paymentInfo.js
+++ a102_fe/src/components/payment/paymentInfo.js
@@ -1,36 +1,50 @@
-import { Col, Row } from "reactstrap";
+import { Col, Row, Button } from "reactstrap";
 import { useState, useEffect } from "react";
 
 function PaymentInfo() {
-    let [cartStorage, setCartStorage] = useState(
-        localStorage.getItem("carts")
-          ? JSON.parse(localStorage.getItem("carts"))
-          : []
-      );
-    
-      const cartItemList = cartStorage.map((cartItem, index) => {
-        let imgurl = `http://i4a102.p.ssafy.io:8080/app/menus/1/${cartItem.itemName}.jpg`;
-        return (
-<Row className="mt-2 paymentInfoItem">
-    <Row className="col-9 paymentMenuInfo">
-      <img src={imgurl} className="d-inline-block col-3" />
-      <div className="col-9 menuItemInfo pt-4 pb-4 pl-0 pr-0 row justify-content-start">
-        <h3 className="col-8">{cartItem.itemName}</h3>
-        <p className="col-4 text-right">{cartItem.itemPrice - 6000}원</p>
-        <p className="col">현재 후원된 그릇 수 : {cartItem.itemAvailable}</p>
-      </div>
+  let [cartStorage, setCartStorage] = useState(
+    localStorage.getItem("carts")
+      ? JSON.parse(localStorage.getItem("carts"))
+      : []
+  );
+
+  const cartItemList = cartStorage.map((cartItem, index) => {
+    let imgurl = `http://i4a102.p.ssafy.io:8080/app/menus/1/${cartItem.itemName}.jpg`;
+    return (
+      <Row className="mt-2 paymentInfoItem justify-content-between" key={index}>
+        <Row className="col-9 paymentMenuInfo">
+          <img src={imgurl} className="d-inline-block col-3" />
+          <div className="col-9 menuItemInfo pt-4 pb-4 pl-0 pr-0 row justify-content-start">
+            <h3 className="col-8">{cartItem.itemName}</h3>
+            <p className="col-4 text-right">{cartItem.itemPrice - 6000}원</p>
+            <p className="col">
+              현재 후원된 그릇 수 : {cartItem.itemAvailable}
+            </p>
+          </div>
+        </Row>
+        <Col xs="3" className="paymentMenuButtons row">
+          <div className="col-9 row justify-content-around">
+            <Button className="paymentCountButton col" color="primary">
+              -
+            </Button>
+            <p className="paymentMenuCount col">{cartItem.itemCount}</p>
+            <Button className="paymentCountButton col" color="primary">
+              +
+            </Button>
+          </div>
+          <div className="col-3 row justify-content-end">
+            <Button className="paymentCountButton" color="danger">
+              x
+            </Button>
+          </div>
+        </Col>
       </Row>
-      <Col xs="3" className="paymentMenuButtons">
-          버튼 영역
-      </Col>
-    </Row>
-        );
-      });
-  
+    );
+  });
 
   return (
-      <Col className="paymentInfoBox pt-2 pb-2" md="8" xs="12">
-    {cartItemList}
+    <Col className="paymentInfoBox pt-2 pb-2" md="8" xs="12">
+      {cartItemList}
     </Col>
   );
 }

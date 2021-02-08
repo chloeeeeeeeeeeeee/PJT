02146e11f41bd14eea4f7dee4e486c9c23bfcef8
jiuen <wldms9604@naver.com>
2021-02-09 00:05:37 +0900

    Edits S04P13A102-128 : 메뉴 Create

--- a102_be/src/main/java/com/ssafy/bab/controller/StoreController.java
+++ a102_be/src/main/java/com/ssafy/bab/controller/StoreController.java
@@ -1,7 +1,5 @@
 package com.ssafy.bab.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -9,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +25,7 @@ import io.swagger.annotations.ApiParam;
 
 @Api("StoreController V1")
 @RestController
-@RequestMapping("/store")
+@RequestMapping(value = "/store")
 public class StoreController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
@@ -41,8 +39,8 @@ public class StoreController {
 	@Autowired
 	private StoreService storeService;
 	
-	@ApiOperation(value = "가게 메뉴 ", notes = "메뉴 이름, 가격, 후원가격, (메뉴 사진)을 입력받아 ", response = List.class)
-	@GetMapping("/item/create")
+	@ApiOperation(value = "가게 메뉴 ", notes = "메뉴 이름, 가격, (후원가격, 메뉴 사진)을 입력받아 ", response = String.class)
+	@PostMapping("/item/create")
 	public ResponseEntity<String> itemCreate(@ApiParam(value = "메뉴 이름, 가격, 후원 가격", required = true) Item item, 
 			@ApiParam(value = "메뉴 이름, 메뉴 가격, 후원 가격", required = false) @RequestParam(value="file", required = false) MultipartFile file,
 			HttpServletRequest req) throws Exception {
@@ -50,10 +48,19 @@ public class StoreController {
 		
 		String jwt = req.getHeader("token");
         int userSeq = jwtService.decode(jwt);
+        User user = userDao.findByUserSeq(3);
         
-        User user = userDao.findByUserSeq(userSeq);
+        // user 정보가 없거나, 가게 사장이 아니거나, 메뉴 이름 또는 가격이 없다면 잘못된 요청임
+		if(user == null || user.getStore() == null || item.getItemName() == null || item.getItemPrice() == null) {
+			return new ResponseEntity<String>("FAIL", HttpStatus.BAD_REQUEST);
+		}
+        
+        item.setStoreId(user.getStore().getStoreId());
+     
+        String result = storeService.itemCreate(item, file);
+        if(result == "SUCCESS") return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        else return new ResponseEntity<String>("FAIL", HttpStatus.INTERNAL_SERVER_ERROR);
 		
-		return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 	}
 	
 }
--- a102_be/src/main/java/com/ssafy/bab/service/StoreServiceImpl.java
+++ a102_be/src/main/java/com/ssafy/bab/service/StoreServiceImpl.java
@@ -1,7 +1,6 @@
 package com.ssafy.bab.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.UUID;
 
 import org.apache.commons.io.FilenameUtils;
@@ -30,6 +29,9 @@ public class StoreServiceImpl implements StoreService {
 	public String itemCreate(Item item, MultipartFile uploadFile) {
 		
 		try {
+			
+			String savingFileName = null;
+			
 			// 업로드한 사진이 있을 경우
 			if(uploadFile != null && !uploadFile.isEmpty()) {
 				File uploadDir = new File(uploadPath);
@@ -43,16 +45,27 @@ public class StoreServiceImpl implements StoreService {
 				//file extension
 				String extension = FilenameUtils.getExtension(fileName);
 				 
-				String savingFileName = uuid + "." + extension;
+				savingFileName = uuid + "." + extension;
 				
 				File destFile = new File(uploadPath + savingFileName);
 				uploadFile.transferTo(destFile);
+			} 
+			// 업로드한 사진이 없을 경우
+			else {
+				savingFileName = "logo.jpg";
 			}
+			
+			// Table Insert
+			item.setItemId(itemDao.getMaxItemId(item.getStoreId()));
+			item.setItemImgUrl("menus/" + savingFileName);
+			itemDao.save(item);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return "FAIL";
 		} 
 		
-		return null;
+		return "SUCCESS";
 	}
 }

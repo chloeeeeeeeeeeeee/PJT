d6e1c0f9da3fee6bacf81e4d151a5fe8600e603e
linibell <linibell@naver.com>
2021-02-09 17:37:41 +0900

    Edits S04P13A102-117, S04P13A102-120 : Menu 수정, 삭제 draft

--- a102_fe/src/pages/store/storeadmin.js
+++ a102_fe/src/pages/store/storeadmin.js
@@ -1,2 +1,32 @@
 // 여기에 메뉴 리스트 등등이 들어가겠지? 나는 여기에다가 delete를 만들 생각이야 그래서 푸쉬를 해보았어.
 // 경로 설정은 안했고, 그냥 파일만 만들어봤어...! 본혁과의 충돌 원하지 않아!
+
+
+// update버튼 이렇게 만들어주세욜~!
+// 주소에 id값이 같이 안와서 이렇게 해야... 내가 id를 빼다 쓸 수가 있어 흑흑
+const Update = (event) => {
+    event.preventDefault();
+    window.history.pushState(menuitem, '아무 이름이나~!', '/menuupdate');  // menuitem : item객체명 바꿔주세욜!
+    window.location.href = '/menuupdate';
+}
+
+// 되겠지... 될거얌 아마! ㅇㅅㅇ!
+// post라 확인을 해볼수가 읎따..! ㅋㅋㅋㅋㅋㅋㅋㅋㅋ안되면 합쳐서 푸쉬해서 청구하세욜~!
+const Delete = (event) => {
+    event.preventDefault();
+    if(window.confirm("정말로 상품을 삭제하시겠습니까? \n삭제된 상품을 복구 할 수 없습니다.")) {
+        fetch(`http://i4a102.p.ssafy.io:8080/app/qna/delete/${menuitem.itemId}`, { // menuitem : item객체명 바꿔주세욜!
+            method: "POST", 
+            headers: {
+                token: localStorage.getItem('access-token'),
+            },
+        }).then(res => {
+            if (res.status === 200) {
+                alert("삭제 성공");
+            }
+            else{
+                alert("실패! 바보 메롱~!~!");
+            }
+        })
+    }
+}

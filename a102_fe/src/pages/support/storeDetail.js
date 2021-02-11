7d5ea6f090edd1120011e342bc0f21198d16bf40
linibell <linibell@naver.com>
2021-02-11 17:36:21 +0900

    Merge Conflict 수정

--- a102_fe/src/pages/support/storeDetail.js
+++ a102_fe/src/pages/support/storeDetail.js
@@ -1,37 +1,4 @@
 import {
-<<<<<<< Updated upstream
-    Container,
-    Row,
-    Col,
-    Card,
-    CardHeader,
-    CardBody,
-    Button,
-  } from "reactstrap";
-  import StoreDetailInfo from "../../components/support/storeDetailInfo";
-  import SupportCart from "../../components/support/supportCart";
-  
-  function StoreDetail(props) {
-    console.log("PROPS");
-    console.log(props);
-    // 매장 ID 값
-    const { storeId } = props.match.params;
-  
-    const testCount = 7;
-  
-    console.log(props.match.params.storeId);
-  
-    return (
-        <Row className="mt-3 storeDetailBox row justify-content-center">
-            <StoreDetailInfo storeInfo={storeId}/>
-            <SupportCart storeInfo={storeId} />
-        </Row>
-    );
-  }
-  
-  export default StoreDetail;
-  
-=======
   Container,
   Row,
   Col,
@@ -65,4 +32,3 @@ function StoreDetail(props) {
 }
 
 export default StoreDetail;
->>>>>>> Stashed changes

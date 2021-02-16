c21c97a07904951f01c7c9a70f2b940e3eb23d38
jiuen <wldms9604@naver.com>
2021-02-16 22:40:12 +0900

    Edits 후원자 암호화 추가

--- a102_be/src/main/java/com/ssafy/bab/dto/Contributor.java
+++ a102_be/src/main/java/com/ssafy/bab/dto/Contributor.java
@@ -2,10 +2,13 @@ package com.ssafy.bab.dto;
 
 import java.io.Serializable;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.ssafy.bab.component.StringCryptoConverter;
 
 import lombok.Data;
 
@@ -15,5 +18,7 @@ public class Contributor implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int contributorSeq;
+	
+	@Convert(converter = StringCryptoConverter.class)
 	private String contributorPhone;
 }
